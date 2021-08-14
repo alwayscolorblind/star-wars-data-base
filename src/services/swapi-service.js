d708c93fd2e885a44ed8a80855b30b2c4e794af9
realpho3nix <55889540+realpho3nix@users.noreply.github.com>
2021-08-14 18:37:40 +0300

    Started Project and Created SwapiService

---
+++ src/services/swapi-service.js
@@ -0,0 +1,46 @@
+export default class SwapiService {
+    _apiBase = 'https://swapi.dev/api';
+
+    async getResource(url) {
+        const response = await fetch(`${this._apiBase}${url}`);
+
+        if (!response.ok) {
+            throw new Error(`Couldn't fetch url: ${url}. Response status: ${response.status}`);
+        }
+
+        return await response.json();
+    }
+
+    async getAllPeople() {
+        const result = await this.getResource(`/people/`);
+        return result.results;
+    }
+
+    getPerson(id) {
+        return this.getResource(`/people/${id}`);
+    }
+
+    async getAllPlanets() {
+        const result = await this.getResource(`/planets/`);
+        return result.results;
+    }
+
+    getPlanet(id) {
+        return this.getResource(`/planets/${id}`);
+    }
+
+    async getAllStarships() {
+        const result = await this.getResource(`/starships/`);
+        return result.results;
+    }
+
+    getStarship(id) {
+        return this.getResource(`/starship/${id}`);
+    }
+}
+
+const api = new SwapiService();
+
+api.getPlanet(2).then((result) => {
+    console.log(result);
+})
